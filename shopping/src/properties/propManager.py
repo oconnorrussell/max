9a2de912a713924e75c0948124827680fa16a90d
Dee <Dee@Dee-PC>
2013-02-03 18:15:33 +0000

    k

--- shopping/src/properties/propManager.py
+++ shopping/src/properties/propManager.py
@@ -1,21 +1,39 @@
 import sys
+from logger import logger
+import os
+
 
 class propManager():
     
     def __init__(self):
-        try:
-            myfile = open('C:\\Users\\Dee\\Dropbox\\python-projects\\rjr-e\\shopping\\props-file\\propertyfile.dat', 'r')
-        except:
-            print "failed to find recipes, aborting"
-            sys.exit
+        propFile = None
+        print 'SM_PROP_DIR = ' + str(os.environ.get('SM_PROP_DIR'))
+        if os.environ.has_key('SM_PROP_DIR'):
+            
+            try:
+                propFile = os.environ.get('PROP_DIR') + '\\propertyfile.dat'
+            except:
+                print 'failed to form propFile, aborting.'
+                sys.exit()
+            
+            else:
+                try:
+                    print 'attempting to open propertyfile.dat =' + propFile
+                    myfile = open(propFile, 'r')
+                except:
+                    logger.err("failed to open propertyfile = " + propFile + '. aborting')
+                    sys.exit()
+                else:
+                    self.properties = {}
+                    property = {}
+                    for line in myfile:
+                        l = line[0:len(line)-1]
+                        elements = l.split('=')
+                        property = {str(elements[0]):str(elements[1])}                
+                        self.properties.update(property)
         else:
-            self.properties = {}
-            property = {}
-            for line in myfile:
-                l = line[0:len(line)-1]
-                elements = l.split('=')
-                property = {str(elements[0]):str(elements[1])}                
-                self.properties.update(property)
-            print self.properties
+            print 'SM_PROP_DIR envvar not defined, aborting'
+            sys.exit()
+    
     def get(self,prop):
         return self.properties.get(prop)
