48e53d21b6878dc71792353af96a701d5a429cc1
Dee <Dee@Dee-PC>
2013-05-04 15:53:33 +0100

    Recovered logger

--- shopping/src/logger/logger.py
+++ shopping/src/logger/logger.py
@@ -1,8 +1,13 @@
 import sys
+import datetime
+
+logfile = open('log_file.txt', 'w')
+
 
 def _callersName():
     #    return the name of the caller
-    return  sys._getframe(2).f_code.co_filename + ':' + str(sys._getframe(2).f_lineno) +  ':' + sys._getframe(2).f_code.co_name
+    #return sys._getframe(2).f_code.co_filename + ':' + str(sys._getframe(2).f_lineno) +  ':' + sys._getframe(2).f_code.co_name + '\n'
+    return    sys._getframe(2).f_code.co_name
 
 def info(msg):
     _info(msg,_callersName())
@@ -11,8 +16,7 @@ def _info(msg, caller=None):
     #    caller is normally not supplied.  when called from within this module, the callers need to say what the actual caller is
     if caller == None:
         caller  =   _callersName()
-    
-    print 'info:' + caller + ':' + msg
+    _print('info:' + caller + ':' + msg)
 
 def started():
     #    issue a started log message
@@ -29,4 +33,12 @@ def _err(msg, caller=None):
     #    caller is normally not supplied.  when called from within this module, the callers need to say what the actual caller is
     if caller == None:
         caller  =   _callersName()
-    print 'err:' + caller + ':' + msg
+    #logfile.write('err' + str(caller) + msg + '\n')
+    _print('err:' + caller + ':' + msg)
+
+def _print(msg, caller = None):
+    now = datetime.datetime.now()
+    _msg = '[' + str(now) + ']' + '' + msg + ']' + '\n'
+    print _msg
+    logfile.write(_msg)
+    
